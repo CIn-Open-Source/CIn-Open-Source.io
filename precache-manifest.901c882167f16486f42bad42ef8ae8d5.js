e25c8ccf845e4817fbc96737b4214b166f3683fb
Gui <ggfl@cin.ufpe.br>
2020-07-27 03:36:38 -0300

    Updates

---
+++ precache-manifest.901c882167f16486f42bad42ef8ae8d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7513b08144b6c8eec4b736d9c68b05c7",
+    "url": "/CIn-Open-Source/index.html"
+  },
+  {
+    "revision": "13e57bc31dca485d5eef",
+    "url": "/CIn-Open-Source/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "25d32887df16d9dfb0a6",
+    "url": "/CIn-Open-Source/static/js/2.53689171.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CIn-Open-Source/static/js/2.53689171.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13e57bc31dca485d5eef",
+    "url": "/CIn-Open-Source/static/js/main.1eb23b4b.chunk.js"
+  },
+  {
+    "revision": "e8b15090a79d4ad89401",
+    "url": "/CIn-Open-Source/static/js/runtime-main.a9160384.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CIn-Open-Source/static/media/logo.5d5d9eef.svg"
+  }
+]);

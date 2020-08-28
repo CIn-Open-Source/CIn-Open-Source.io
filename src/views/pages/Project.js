33f6c6d43040a5dfcc4113f3d872e95b4b3fb27e
Gui <ggfl@cin.ufpe.br>
2020-08-28 12:40:22 -0300

    refactor[remove-unused]: removing unused assets

--- src/views/pages/Project.js
+++ src/views/pages/Project.js
@@ -74,7 +74,7 @@ class Project extends React.Component {
                           <img
                             alt="..."
                             className="rounded-circle"
-                            src={require("assets/img/theme/team-4-800x800.jpg")}
+                            src={require("assets/img/theme/team-guila.jpg")}
                           />
                         </a>
                       </div>
